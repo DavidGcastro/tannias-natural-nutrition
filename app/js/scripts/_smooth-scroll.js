72298e1d6940a9182e7e630e3af766bcb0f33d7f
David <davidgcastro93@gmail.com>
2017-11-21 00:39:47 -0500

    fixed jquery smooth scroll bug

--- app/js/scripts/_smooth-scroll.js
+++ app/js/scripts/_smooth-scroll.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
 
     var headerHeight = $(".site-header").outerHeight();
+    var headerTop = parseInt($('.site-header').css("top"),10);
+    var trueHeight = headerHeight + headerTop;
+    
 
 
     $("nav li a ").on("click", function (e) {
+        
+      
+        console.log(trueHeight);
         var linkHref = $(this).attr('href');
         $('html, body').animate({
-            scrollTop: $(linkHref).offset().top - headerHeight
+            scrollTop: $(linkHref).offset().top - trueHeight
         }, 1000);
 
         e.preventDefault();
