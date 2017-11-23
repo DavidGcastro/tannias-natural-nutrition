b5e560bf19c64fd260816f02ab6d7fc79786d55a
David <davidgcastro93@gmail.com>
2017-11-22 22:01:01 -0500

    finished mobile header

--- app/js/scripts/_smooth-scroll.js
+++ app/js/scripts/_smooth-scroll.js
@@ -1,21 +1,30 @@
 $(document).ready(function () {
-
+    //find the height of nav, since nav is position relative and hasa negative top, i need another var, to find the
+    //value of the top and parese that into an integer
+    //then i hadd the height of the nav as detected by outerheight and add it with the value of my top
     var headerHeight = $(".site-header").outerHeight();
     var headerTop = parseInt($('.site-header').css("top"),10);
     var trueHeight = headerHeight + headerTop;
     
 
-
+    // when u click on a link in the nac
     $("nav li a ").on("click", function (e) {
+         $("nav li a").css("color", "#575756");
+         $(this).css("color", "#88c1ba")
         
-      
-        console.log(trueHeight);
+        
         var linkHref = $(this).attr('href');
+        //clicked nav is equal to this, we set the href to a variable
+       
+        
+        
         $('html, body').animate({
+            //we select the dom element that has the same id or class as the clicked anchors tags href
             scrollTop: $(linkHref).offset().top - trueHeight
-        }, 1000);
+            //scroll to the element clicked, BUT get the coordinate of the divs top position minus the height of the nav, to prevent overlap
+        }, 1000); //slow it down
 
-        e.preventDefault();
+        e.preventDefault(); //prevent the defualt fast scroll
     });
 
 });
