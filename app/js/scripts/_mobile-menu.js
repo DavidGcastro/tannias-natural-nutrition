b4e9ed6380a08c824730dbb1ae3e9d7900095c5c
David <davidgcastro93@gmail.com>
2017-11-23 15:27:22 -0500

    finished mobile functionality

--- app/js/scripts/_mobile-menu.js
+++ app/js/scripts/_mobile-menu.js
@@ -4,15 +4,9 @@
     var addMe = document.getElementsByClassName("content-medium");
 
 
+    $(menuIcon).on("click", function () {
+        $(content).slideToggle(400, function () {
+            $(this).toggleClass("content-medium").css("display", "");
+        })
 
-        $(menuIcon).on("click", function () {
-    
-        
-            $(content).slideToggle(300, function () {
-                $(this).toggleClass("content-medium").css("display", "");
-            })
-           
-        });
-
-
-    
+    });
--- app/js/scripts/_smooth-scroll.js
+++ app/js/scripts/_smooth-scroll.js
@@ -8,14 +8,24 @@ $(document).ready(function () {
     var content = document.getElementsByClassName("site-header--content");
 
 
+
+
     // when u click on a link in the nav
     $("nav li a ").on("click", function (e) {
         $("nav li a").css("color", "#575756");
-        $(this).css("color", "#88c1ba")
-        $(content).slideUp(500, function () {
-            $(this).toggleClass("content-medium").css("display", "");
+        $(this).css("color", "#88c1ba");
+        
+        // stop the damn mobile nav behavior on 768 and above!
+        var windowWidth = $(window).width();
+        if (windowWidth < 768) {
+            $(content).slideUp(500, function () {
+                $(this).toggleClass("content-medium").css("display", "");
 
-        });
+            });
+
+        }
+
+
 
         var linkHref = $(this).attr('href');
         //clicked nav is equal to this, we set the href to a variable
