56f65c6e4bdd7d6932b59ee6243a5f05e4f78085
David <davidgcastro93@gmail.com>
2017-11-22 22:35:41 -0500

    mobile nav bug fix

--- app/js/scripts/_mobile-menu.js
+++ app/js/scripts/_mobile-menu.js
@@ -3,10 +3,11 @@ $(document).ready(function () {
     var menuIcon = document.getElementsByClassName("site-header--close-icon");
     var siteHeader = document.getElementsByClassName("site-header");
     var content = document.getElementsByClassName("site-header--content");
-    $(content).slideToggle(300);
 
     $(menuIcon).on("click", function () {
-        $(content).slideToggle(300);
+        $(content).slideToggle(300, function(){
+           $(this).toggleClass("content-medium").css("display", "");
+        });
 
     });
 
