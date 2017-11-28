4757fb9b6b06e877f6b487a449059e576027188e
David <davidgcastro93@gmail.com>
2017-11-27 21:19:47 -0500

    fixed back to top bug

--- app/js/scripts/_back-to-top.js
+++ app/js/scripts/_back-to-top.js
@@ -1,23 +1,34 @@
 var backToTop = document.getElementById("back-to-top");
-
+var footer = $(".footer").outerHeight();
+var top = $(".footer").scrollTop();
 
 
 $(window).scroll(function () {
-	if ($(window).scrollTop() > 800) {
-		$(backToTop).fadeIn('fast');
-	} else {
-		$(backToTop).fadeOut('fast');
+    console.log($(window).height());
+    console.log($(window).scrollTop());
+    console.log($(document).height());
+
+    if ($(window).scrollTop() > 800 && $(window).height() + $(window).scrollTop() < $(document).height() - footer) {
+        $(backToTop).fadeIn('fast');
+    } else {
+        $(backToTop).fadeOut('fast');
     }
 });
 
 
 
-$(backToTop).click(function() {
-	$('html, body').animate({
-		scrollTop: 0
-	}, 700, 'swing');
-	return false;
+$(backToTop).click(function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700, 'swing');
+    return false;
 });
 
 
-alert($(".footer").outerHeight())
+
+//$(window).scroll(function () {
+//    // Check if the window is almost at the bottom (-230 for the height of the footer)
+//    if ($(window).height() + $(window).scrollTop() >= $(document).height() - footer) {
+//        console.log("goo");
+//    }
+//});
