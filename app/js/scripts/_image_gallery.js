4785a48c3a9bb3d3f5ba29728b2d7667bc15eecc
David <davidgcastro93@gmail.com>
2017-12-31 20:50:02 -0500

    added arrow functionality, and logic

--- app/js/scripts/_image_gallery.js
+++ app/js/scripts/_image_gallery.js
@@ -1,38 +1,70 @@
 var image = $('.img-gallery--imgWrapper');
 var modalImage = $('#myModal');
-var close =  $('.closeImageGallery');
+var close = $('.closeImageGallery');
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var displayCaption = $('#caption')
-var captionText = $('.img-gallery--img--textContainer').html();
 var left = $('#left');
 var right = $('#right');
+var captionText = $('.img-gallery--img--textContainer');
 var imgSrc;
-
+var index;
+var caption;
 
 image.on('click', function () {
-//    e.stopPropagation();
-  imgSrc = $(this).find("img").attr('src');
+
+    index = ($(image).index(this));
+    imgSrc = $(this).find("img").attr('src');
+    caption = $(captionText[index]).html();
     $(modalImage).css('display', 'block')
     $('#img01').attr('src', imgSrc)
-    $(displayCaption).html(captionText)
+    $(displayCaption).html(caption)
+
+    $(right).css('display', 'block');
+    $(left).css('display', 'block');
+
+    if (index == image.length - 1) {
+        $(right).css('display', 'none');
+    }
+
+
+    if (index == 0) {
+        $(left).css('display', 'none');
+    }
 })
 
 
-
-close.on('click', function (){
+close.on('click', function () {
     $(modalImage).css('display', 'none')
 })
 
+$(left).on('click', function () {
+    console.log(index)
+    if (index !== 0) {
+        index--;
+        caption = $(captionText[index]).html();
+        var next = ($(image[index]).find("img").attr('src'));
+        $('#img01').attr('src', next);
+        $(displayCaption).html(caption)
+    }
+    if (index === 0) {
+
+        $(this).css("display", "none");
+    }
+
+    $(right).css('display', 'block')
+});
 
 
-$(left).on('click', function(){
-    
+$(right).on('click', function () {
+    console.log(index)
+    $(left).css('display', 'block');
+    if (index == image.length - 2) {
+        $(right).css('display', 'none');
+    }
+    index++;
+    var next = ($(image[index]).find("img").attr('src'));
+    $('#img01').attr('src', next)
+    caption = $(captionText[index]).html();
+    $(displayCaption).html(caption)
 })
-
-
-$(right).on('click', function(){
-  console.log(imgSrc);
-})
-
-
